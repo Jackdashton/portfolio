70bb3058c88d05e7bb61ee6f06705288df7a5b19
jack ashton <jackdashton@outlook.com>
2023-07-11 22:56:57 +0100

    tailwind and react router added

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// React Router V4 - Browser Router
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
