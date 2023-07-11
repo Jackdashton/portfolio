193f138ed9b580b5606558ade54c64ec3d89189f
jack ashton <jackdashton@outlook.com>
2023-07-11 22:36:52 +0100

    folder structure and components created

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import TechStack from './components/TechStack'
+
+
+function App() {
+
+  return (
+    <div>
+      New App
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Home/>
+      <NavBar/>
+      <Projects/>
+      <TechStack/>
+    </div>
+  )
+}
+
+export default App
