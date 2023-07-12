f5fea63a32cab58927229b6c7ffefa520a4d553b
jack ashton <jackdashton@outlook.com>
2023-07-12 12:34:15 +0100

    routing added to single pages

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 // React Router V4 - Browser Router
 import { BrowserRouter } from "react-router-dom";
+// Router accepts one child, therefore we pass it in main.jsx
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* Wrap app in component for info related to routing to other components */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
