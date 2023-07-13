a9b3d559150887825ae2d2ab8c4f89eb6a7ad68f
jack ashton <jackdashton@outlook.com>
2023-07-13 23:05:26 +0100

    Added footer, CSS styling to About, and About content

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import styles from './About.module.css'
 
 function About() {
   return (
     <>
-    <h1>About</h1>
-    <p>Hi, my name is Jack Ashton. I'm a former structural engineer who has recently finished a
-      Fullstack Web Development Course. 
-    </p>
+    <div className={styles.wrapper}>
+      <h1>Hi, my name is Jack Ashton.</h1>
+
+      <img className={styles.image} src='/public/images/profile1.jpg' alt="Image of Jack Ashton" />
+
+      <p>
+        I'm a former structural engineer who has recently finished a
+        Fullstack Web Development Course. I am now actively seeking a role where I can
+        leverage my technical background, transferable skills, and passion for coding to make a
+        meaningful impact as a full-stack developer. As a former structural engineer,
+        I gained valuable technical expertise and developed transferable skills. Throughout my engineering career,
+        I refined my ability to approach challenges in a methodical and analytical manner.
+        Recently I have completed the Le Wagon Web Development Bootcamp which covered the core concepts of Object-orientated
+        programming through both front and backend development.
+        My skills include Ruby/Ruby on Rails, JavaScript, HTML, CSS and SQL.
+      </p>
+    </div>
     </>
   )
 
