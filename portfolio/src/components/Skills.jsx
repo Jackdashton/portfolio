e07c7313139ea6903d1bffb351dc2ea78cff09cb
jack ashton <jackdashton@outlook.com>
2023-07-13 16:28:25 +0100

    navbar styling added and css file connected

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <>
+    <h1>Skills</h1>
+
+    <h2>Front End</h2>
+    <p>JavaScript</p>
+    <p>HTML</p>
+    <h2>Back End</h2>
+    <h2>Soft Skills</h2>
+    <h2>Other Tech Skills</h2>
+    </>
+  )
 }
 
 export default Skills;
