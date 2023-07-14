7d3aa2dd238d31d94638ccb871b847c23246db6f
jack ashton <jackdashton@outlook.com>
2023-07-14 16:31:41 +0100

    Github icon added through SVG brands

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Footer.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { faGithub } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+// faCoffee is icon name, import from solid svg icons
+
 
 function Footer() {
-
-  const element = <FontAwesomeIcon icon={faCoffee} />
 
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.links}>
-          {element};
           <p>© 2023 Jack Ashton </p>
+          <FontAwesomeIcon icon={faGithub} />
           <a href="https://github.com/Jackdashton">Github</a>
           <a href="https://linkedin.com/in/jack-david-ashton">LinkedIn</a>
         </div>
