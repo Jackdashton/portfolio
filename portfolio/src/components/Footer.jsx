71ee7da1c741dec08da1e32abde8bde346cf7d24
jack ashton <jackdashton@outlook.com>
2023-07-14 15:54:29 +0100

    footer sticky, font awesome files added

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './Footer.module.css'
 
 function Footer() {
   return (
     <>
-      <h1>Footer</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.links}>
+          <a href="https://github.com/Jackdashton">Github</a>
+          <a href="https://linkedin.com/in/jack-david-ashton">LinkedIn</a>
+        </div>
+      </div>
     </>
   )
 }
