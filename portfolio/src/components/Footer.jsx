e87a0bfcc370cd26b10cd400ba50d193ed8bfa0b
jack ashton <jackdashton@outlook.com>
2023-07-14 15:58:05 +0100

    font awesome icon added to footer for test

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
+
+  const element = <FontAwesomeIcon icon={faCoffee} />
+
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.links}>
+          {element};
+          <p>© 2023 Jack Ashton </p>
           <a href="https://github.com/Jackdashton">Github</a>
           <a href="https://linkedin.com/in/jack-david-ashton">LinkedIn</a>
         </div>
