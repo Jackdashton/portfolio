2c88bf61efcec67c580187299546dbb7fa396b03
jack ashton <jackdashton@outlook.com>
2023-07-17 23:41:22 +0100

    added html and styling for contact page

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import styles from "./Contact.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
     <>
-      <h1>Contact</h1>
+      <h1>GET IN <span className={styles.specialWord}>TOUCH</span></h1>
+      <div className={styles.wrapper}>
+        <div className={styles.linkedin}>
+          <h2>LinkedIn</h2>
+          <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+          <br />
+          LinkedIn:
+          <br />
+          <a href="https://www.linkedin.com/in/jack-david-ashton/">https://www.linkedin.com/in/jack-david-ashton/</a>
+        </div>
+        <div className={styles.email}>
+          <h2>Email</h2>
+          <FontAwesomeIcon icon={faEnvelope} size="2xl" />
+          <br />
+          Email:
+          <br />
+          Jackdashton@outlook.com
+        </div>
+        <div className={styles.phone}>
+          <h2>Phone</h2>
+          <FontAwesomeIcon icon={faPhone} size="2xl" />
+          <br />
+          Please use email or LinkedIn for initial contact. A telphone number
+          will then be provided.
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Contact;
