351d928eb577fceab2eda46bbd05405cc26990ed
jack ashton <jackdashton@outlook.com>
2023-07-18 15:36:03 +0100

    styling added to Skills and SkillIcon

--- portfolio/src/components/SkillIcon.jsx
+++ portfolio/src/components/SkillIcon.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './SkillIcon.module.css'
 
 function SkillIcon( {name, logo} ) {
   return (
     <>
       <p>{name}</p>
-      <button>
-        <img src={"/public/images/icons/" +logo +".png"} alt="javascript logo" />
+      <button className={styles.icon}>
+        <img src={"/public/images/icons/" + logo +".png"} alt="javascript logo" />
       </button>
     </>
   )
--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SkillIcon from './SkillIcon';
+import styles from './Skills.module.css'
 
 function Skills() {
   return (
@@ -7,12 +8,20 @@ function Skills() {
     <h1>Skills</h1>
     <SkillIcon name={"Ruby"} logo={"Ruby"}/>
     <SkillIcon name={"JavaScript"} logo={"JavaScript"}/>
-    <h2>Front End</h2>
-    <p>JavaScript</p>
-    <p>HTML</p>
-    <h2>Back End</h2>
-    <h2>Soft Skills</h2>
-    <h2>Other Tech Skills</h2>
+    <div>
+      <div className={styles.frontend}>
+      <h2>Front End</h2>
+      </div>
+      <div className={styles.backend}>
+      <h2>Back End</h2>
+      </div>
+      <div className={styles.softskills}>
+      <h2>Soft Skills</h2>
+      </div>
+      <div className={styles.other}>
+      <h2>Other Tech</h2>
+      </div>
+    </div>
     </>
   )
 }
