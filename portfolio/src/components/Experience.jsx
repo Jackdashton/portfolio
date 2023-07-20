14ef674c71e472f4ab5c61593c68298cd8051a4e
jack ashton <jackdashton@outlook.com>
2023-07-20 22:33:52 +0100

    added ProjectCard component

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -4,8 +4,30 @@ function Experience() {
   return (
     <>
       <h1>Experience</h1>
+      <div className="wrapper">
+        <div className="jobCard">
+          <dl>
+            <dt>Job</dt>
+            <dd>Frontend Developer</dd>
+            <dt>Date</dt>
+            <dd>April 2023 - Present</dd>
+            <dt>Role</dt>
+            <dd>Primarily my tasks were WordPress development</dd>
+          </dl>
+        </div>
+        <div className="jobCard">
+        <dl>
+            <dt>Job</dt>
+            <dd>Structural Engineer</dd>
+            <dt>Date</dt>
+            <dd>September 2019 - October 2022</dd>
+            <dt>Role</dt>
+            <dd>Primarily my tasks were structural engineering</dd>
+          </dl>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Experience;
