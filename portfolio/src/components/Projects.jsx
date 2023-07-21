26ad962e1f63c1cfbf1cf365d9d200c8c0d5a5c5
jack ashton <jackdashton@outlook.com>
2023-07-21 10:38:01 +0100

    card functionality added

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import { projectData } from "../projects.data.js";
+import styles from './Projects.module.css'
 
 function Projects() {
   return (
     <>
       <h1>Projects</h1>
-      <ProjectCard/>
-      <ProjectCard/>
-      <ProjectCard/>
+      <div className={styles.wrapper}>
+        {projectData.map((project) => (
+          <div className={styles.projectCard} key={project.id}>
+            {project.images?.map((type, index) => {
+              return (
+                <p key={index}>{type}</p>
+              )
+            })}
+            <h2>{project.title}</h2>
+            <p>Tech Stack: {project.tech}</p>
+            <p>Projects: {project.description}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
 export default Projects;
+
+{/* <img src={`/images/screenshots/t-rex screenshot2.JPG`} alt="" /> */}
--- portfolio/src/projects.data.js
+++ portfolio/src/projects.data.js
@@ -1,6 +1,21 @@
 export const projectData = [
   {
+    id: crypto.randomUUID(),
     title: 'Get your T-rex',
     tech: 'Ruby on Rails',
+    description: 'This is a project.',
+    images: ["t-rex screenshot1","t-rex screenshot2"],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Get your T-rex',
+    tech: 'Ruby on Rails',
+    description: 'This is a project.',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Get your T-rex',
+    tech: 'Ruby on Rails',
+    description: 'This is a project.',
   },
 ]
