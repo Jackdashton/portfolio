1345b143a0933c9125210d50c3c211170c3c9ffa
jack ashton <jackdashton@outlook.com>
2023-07-24 18:37:39 +0100

    Basic information working for one project

--- portfolio/src/components/projects/Cabinn.jsx
+++ portfolio/src/components/projects/Cabinn.jsx
@@ -2,9 +2,34 @@ import React from "react";
 import { projectData } from "../../projects.data.js";
 
 function Cabinn() {
+
+  function findArrayElementByTitle( title) {
+    return projectData.find((element) => {
+      return element.title === title;
+    });
+  }
+
+  const project = findArrayElementByTitle("CabInn")
+
+  if (!project) {
+    return"Not found"
+  }
+
   return (
     <>
       <h1>CabInn</h1>
+      {project.title}
+      {project.tech}
+      {project.description}
+      {project.images?.map((type, index) => {
+              return (
+                <ul key={index}>
+                  <li>
+                    {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
+                  </li>
+                </ul>
+              );
+            })}
     </>
   );
 }
