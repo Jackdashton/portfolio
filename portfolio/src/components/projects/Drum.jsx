6bd898dc76a8ab88613daa87e61e9611eb55be6f
jack ashton <jackdashton@outlook.com>
2023-07-24 19:01:45 +0100

    All projects added to data file, need to be updated

--- portfolio/src/components/projects/Drum.jsx
+++ portfolio/src/components/projects/Drum.jsx
@@ -2,9 +2,38 @@ import React from "react";
 import { projectData } from "../../projects.data.js";
 
 function Drum() {
+  function findArrayElementByTitle( title) {
+    return projectData.find((element) => {
+      return element.title === title;
+    });
+  }
+
+  const project = findArrayElementByTitle("Drumkit")
+
+  if (!project) {
+    return (
+      <>
+        <h1>Drumkit</h1>
+        <p>Project not found!</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>Drumkit</h1>
+      {project.title}
+      {project.tech}
+      {project.description}
+      {project.images?.map((type, index) => {
+              return (
+                <ul key={index}>
+                  <li>
+                    {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
+                  </li>
+                </ul>
+              );
+            })}
     </>
   );
 }
--- portfolio/src/projects.data.js
+++ portfolio/src/projects.data.js
@@ -14,7 +14,10 @@ export const projectData = [
     tech: "Ruby on Rails, JavaScript, HTML, CSS, Stimulus",
     description:
       "Took the lead in ensuring that the API data was accurately consumed on the front-end, focusing particularly on its responsiveness to deliver a seamless user experience. My efforts resulted in an improved, efficient, and user-friendly interface. Performed additional work on the frontend of the app, this allowed us to ensure a better user experience.",
-    images: ["movie2night screenshot 1", "movie2night screenshot 4, movie2night screenshot 5"],
+    images: [
+      "movie2night screenshot 1",
+      "movie2night screenshot 4, movie2night screenshot 5",
+    ],
   },
   {
     id: crypto.randomUUID(),
@@ -26,8 +29,32 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: "CabInn",
+    title: "Drumkit",
     tech: "Ruby on Rails, JavaScript, HTML, CSS",
+    description:
+      "I designed and developed a web-based marketplace application which made use of MVC architecture. This app allows a user to create an account using password authentication and an avatar as well as CRUD functionality based on user permissions for functions such as adding properties, bookings and reviews. The project was my first solo project after the bootcamp and it allowed me to practice backend development using Ruby on Rails and database migration. Additionally, as I was responsible for the frontend as well, I was able to hone my CSS, HTML and Figma abilities during the project. The geolocation feature and database interdependencies were the most challenging aspects of the project.",
+    images: ["t-rex screenshot1", "t-rex screenshot2"],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Escape the Owl",
+    tech: "Ruby on Rails, JavaScript, HTML, CSS",
+    description:
+      "I designed and developed a web-based marketplace application which made use of MVC architecture. This app allows a user to create an account using password authentication and an avatar as well as CRUD functionality based on user permissions for functions such as adding properties, bookings and reviews. The project was my first solo project after the bootcamp and it allowed me to practice backend development using Ruby on Rails and database migration. Additionally, as I was responsible for the frontend as well, I was able to hone my CSS, HTML and Figma abilities during the project. The geolocation feature and database interdependencies were the most challenging aspects of the project.",
+    images: ["t-rex screenshot1", "t-rex screenshot2"],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Task List",
+    tech: "Ruby on Rails, JavaScript, HTML, CSS",
+    description:
+      "I designed and developed a web-based marketplace application which made use of MVC architecture. This app allows a user to create an account using password authentication and an avatar as well as CRUD functionality based on user permissions for functions such as adding properties, bookings and reviews. The project was my first solo project after the bootcamp and it allowed me to practice backend development using Ruby on Rails and database migration. Additionally, as I was responsible for the frontend as well, I was able to hone my CSS, HTML and Figma abilities during the project. The geolocation feature and database interdependencies were the most challenging aspects of the project.",
+    images: ["t-rex screenshot1", "t-rex screenshot2"],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Structural Engineering",
+    tech: "Bluebeam, Tekla Structural Designer, Tedds, Autocad",
     description:
       "I designed and developed a web-based marketplace application which made use of MVC architecture. This app allows a user to create an account using password authentication and an avatar as well as CRUD functionality based on user permissions for functions such as adding properties, bookings and reviews. The project was my first solo project after the bootcamp and it allowed me to practice backend development using Ruby on Rails and database migration. Additionally, as I was responsible for the frontend as well, I was able to hone my CSS, HTML and Figma abilities during the project. The geolocation feature and database interdependencies were the most challenging aspects of the project.",
     images: ["t-rex screenshot1", "t-rex screenshot2"],
