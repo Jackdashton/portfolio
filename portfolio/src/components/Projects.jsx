d4759d2e8a482e0b098688f33a4a74d48bfaf39b
jack ashton <jackdashton@outlook.com>
2023-07-26 22:23:03 +0100

    fixed project cards

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProjectCard from "./ProjectCard";
 import { projectData } from "../projects.data.js";
 import styles from "./Projects.module.css";
 
@@ -14,18 +13,10 @@ function Projects() {
             key={project.id}
             style={{
               backgroundImage: `url(/images/screenshots/${project.images[0]}.JPG)`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              width: "100%",
-              height: "100%",
-              backgroundPosition: "center",
             }}
-
           >
             {project.images?.length > 0 && (
-              <>
                 <a href={`/${project.link}`}>{project.title}</a>
-              </>
             )}
           </div>
         ))}
