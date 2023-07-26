aee44228e102b9d4ff447a49714bf71c552d2918
jack ashton <jackdashton@outlook.com>
2023-07-26 12:49:49 +0100

    created Homepage

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import styles from './Home.module.css'
 
 function Home() {
   return (
-    <>
-    <h1>Home</h1>
-    </>
-  )
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: "url(images/background.png)",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100%",
+        backgroundPosition: "center",
+        opacity: "10%",
+      }}
+    >
+    <h1>Welcome</h1>
+    </div>
+  );
 }
 
 export default Home;
