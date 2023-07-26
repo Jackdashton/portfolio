f1de8bb57eb1a8efb997baec566660b82ad2c96c
jack ashton <jackdashton@outlook.com>
2023-07-26 22:38:26 +0100

    footer fixed and movie added

--- portfolio/src/components/projects/Trex.jsx
+++ portfolio/src/components/projects/Trex.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { projectData } from "../../projects.data.js";
-import styles from "./Trex.module.css"
+import styles from "./Trex.module.css";
 
 function Trex() {
   function findArrayElementByTitle(title) {
@@ -18,21 +18,18 @@ function Trex() {
       <div className={styles.wrapper}>
         <div className={styles.header}>{project.title}</div>
         <div className={styles.info}>
-          <p>Tech Stack:</p>{project.tech}
+          <p>Tech Stack:</p>
+          {project.tech}
           <p>Description:</p>
           <p>
-            Cabinn is a marketplace app for finding a rental cabin at a holiday
-            destination of your choosing. The application allows a user to see
-            all of the available cabins for rental, as well as see further
-            details of an individual property. The user can then book a cabin,
-            or leave a review for a cabin they have already booked. As a user
-            you can also add a cabin to the database. The project was undertaken
-            to improve and add to the fundamentals of Ruby on Rails and general
-            web development.
+            This was the first app that I worked on in the Bootcamp. We worked
+            in an interdisciplinary team of four, dividing the work between
+            ourselves in order to put in to practice the fundamentals which we'd
+            been learning on the course. My primary task was the backend of the
+            website; creating the database using SQLite as well as creating and
+            testing the user registration and login features.
             <br />
             <br />
-            The project included Ruby gems such as Mapbox geolocation as well as
-            unit testing.
           </p>
         </div>
         <div className={styles.images}>
