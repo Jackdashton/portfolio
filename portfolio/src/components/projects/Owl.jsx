87acdc0d0b4fe58b7eb5cbde8a07d35a2774c8da
jack ashton <jackdashton@outlook.com>
2023-07-26 22:47:38 +0100

    all project files updated

--- portfolio/src/components/projects/Owl.jsx
+++ portfolio/src/components/projects/Owl.jsx
@@ -1,38 +1,51 @@
 import React from "react";
 import { projectData } from "../../projects.data.js";
+import styles from "./Owl.module.css";
 
 function Owl() {
-  function findArrayElementByTitle( title) {
-    return projectData.find((element) => {
-      return element.title === title;
-    });
+  function findArrayElementByTitle(title) {
+    return projectData.find((element) => element.title === title);
   }
 
-  const project = findArrayElementByTitle("Escape the Owl")
+  const project = findArrayElementByTitle("Escape the Owl");
 
   if (!project) {
-    return (
-      <>
-        <h1>Escape the Owl</h1>
-        <p>Project not found!</p>
-      </>
-    )
+    return "Not found";
   }
 
   return (
     <>
-      <h1>Escape the Owl</h1>
-      {project.title}
-      {project.tech}
-      {project.images?.map((type, index) => {
-              return (
-                <ul key={index}>
-                  <li>
-                    {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
-                  </li>
-                </ul>
-              );
-            })}
+      <div className={styles.wrapper}>
+        <div className={styles.header}>{project.title}</div>
+        <div className={styles.info}>
+          <p>Tech Stack:</p>
+          {project.tech}
+          <p>Description:</p>
+          <p>
+            Developed a web-based vocabulary quiz game using JavaScript, HTML
+            and CSS. The app allows users to play a vocabulary quiz game by
+            answering from a series of multiple-choice questions. The app also
+            includes a visual representation of the user’s progress in order to
+            gamify the quiz; moving either the user or the owl depending on
+            whether the answer is correct, with the aim to escape the owl. The
+            app was a great opportunity to practice DOM manipulation and the
+            fundamentals of JavaScript as well as work on frontend design using
+            CSS and HTML.
+            <br />
+          </p>
+        </div>
+        <div className={styles.images}>
+          {project.images?.map((type, index) => {
+            return (
+              <ul key={index}>
+                <li>
+                  {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- portfolio/src/components/projects/Task.jsx
+++ portfolio/src/components/projects/Task.jsx
@@ -1,38 +1,44 @@
 import React from "react";
 import { projectData } from "../../projects.data.js";
+import styles from "./Task.module.css";
 
 function Task() {
-  function findArrayElementByTitle( title) {
-    return projectData.find((element) => {
-      return element.title === title;
-    });
+  function findArrayElementByTitle(title) {
+    return projectData.find((element) => element.title === title);
   }
 
-  const project = findArrayElementByTitle("Task List")
+  const project = findArrayElementByTitle("Task List");
 
   if (!project) {
-    return (
-      <>
-        <h1>Task List</h1>
-        <p>Project not found!</p>
-      </>
-    )
+    return "Not found";
   }
 
   return (
     <>
-      <h1>Task List</h1>
-      {project.title}
-      {project.tech}
-      {project.images?.map((type, index) => {
-              return (
-                <ul key={index}>
-                  <li>
-                    {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
-                  </li>
-                </ul>
-              );
-            })}
+      <div className={styles.wrapper}>
+        <div className={styles.header}>{project.title}</div>
+        <div className={styles.info}>
+          <p>Tech Stack:</p>
+          {project.tech}
+          <p>Description:</p>
+          <p>
+            Creation of a Task list app to improve JavaScript fundamentals as
+            well as CSS and HTML front-end capabilities.
+            <br />
+          </p>
+        </div>
+        <div className={styles.images}>
+          {project.images?.map((type, index) => {
+            return (
+              <ul key={index}>
+                <li>
+                  {<img src={`/images/screenshots/${type}.JPG`} alt="" />}
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
