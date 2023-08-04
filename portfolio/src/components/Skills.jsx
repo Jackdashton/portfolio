647b4a5ad45945155863617dcad231108151db49
jack ashton <jackdashton@outlook.com>
2023-08-04 17:51:13 +0100

    mobile for Skills page done

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -17,130 +17,136 @@ function Skills() {
     <>
       <h1>Skills</h1>
       <div className={styles.wrapper}>
-        <div className={styles.column}>
-          <h2>Front End</h2>
-          <div className={styles.icons}>
-            <SkillIcon
-              id={"js"}
-              name={"JavaScript"}
-              logo={"JavaScript"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"html"}
-              name={"HTML"}
-              logo={"HTML"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"css"}
-              name={"CSS"}
-              logo={"CSS"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"react"}
-              name={"React"}
-              logo={"React"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"stimulus"}
-              name={"Stimulus"}
-              logo={"Stimulus"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"wordpress"}
-              name={"WordPress"}
-              logo={"WordPress"}
-              onClick={handleSkillClick}
-            />
+        <div className={styles.skills}>
+          <div className={styles.column}>
+            <h2>Front End</h2>
+            <div className={styles.icons}>
+              <SkillIcon
+                id={"js"}
+                name={"JavaScript"}
+                logo={"JavaScript"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"html"}
+                name={"HTML"}
+                logo={"HTML"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"css"}
+                name={"CSS"}
+                logo={"CSS"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"react"}
+                name={"React"}
+                logo={"React"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"stimulus"}
+                name={"Stimulus"}
+                logo={"Stimulus"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"wordpress"}
+                name={"WordPress"}
+                logo={"WordPress"}
+                onClick={handleSkillClick}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.column}>
-          <h2>Back End</h2>
-          <div className={styles.icons}>
-            <SkillIcon
-              id={"ruby"}
-              name={"Ruby"}
-              logo={"Ruby"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"ror"}
-              name={"Ruby on Rails"}
-              logo={"RubyOnRails"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"python"}
-              name={"Python"}
-              logo={"Python"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"sql"}
-              name={"SQL"}
-              logo={"SQL"}
-              onClick={handleSkillClick}
-            />
+          <div className={styles.column}>
+            <h2>Back End</h2>
+            <div className={styles.icons}>
+              <SkillIcon
+                id={"ruby"}
+                name={"Ruby"}
+                logo={"Ruby"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"ror"}
+                name={"Ruby on Rails"}
+                logo={"RubyOnRails"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"python"}
+                name={"Python"}
+                logo={"Python"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"sql"}
+                name={"SQL"}
+                logo={"SQL"}
+                onClick={handleSkillClick}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.column}>
-          <h2>Other Tech</h2>
-          <div className={styles.icons}>
-            <SkillIcon
-              id={"figma"}
-              name={"Figma"}
-              logo={"Figma"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"hero"}
-              name={"Heroku"}
-              logo={"Heroku"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"term"}
-              name={"Terminal"}
-              logo={"Terminal"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"gh"}
-              name={"GitHub"}
-              logo={"GitHub"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"blue"}
-              name={"Bluebeam"}
-              logo={"Bluebeam"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"auto"}
-              name={"Autocad"}
-              logo={"Autocad"}
-              onClick={handleSkillClick}
-            />
-            <SkillIcon
-              id={"tds"}
-              name={"Tekla Design Suite"}
-              logo={"Tekla"}
-              onClick={handleSkillClick}
-            />
+          <div className={styles.column}>
+            <h2>Other Tech</h2>
+            <div className={styles.icons}>
+              <SkillIcon
+                id={"figma"}
+                name={"Figma"}
+                logo={"Figma"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"hero"}
+                name={"Heroku"}
+                logo={"Heroku"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"term"}
+                name={"Terminal"}
+                logo={"Terminal"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"gh"}
+                name={"GitHub"}
+                logo={"GitHub"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"blue"}
+                name={"Bluebeam"}
+                logo={"Bluebeam"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"auto"}
+                name={"Autocad"}
+                logo={"Autocad"}
+                onClick={handleSkillClick}
+              />
+              <SkillIcon
+                id={"tds"}
+                name={"Tekla Design Suite"}
+                logo={"Tekla"}
+                onClick={handleSkillClick}
+              />
+            </div>
           </div>
         </div>
         <div className={styles.column} id={styles.skillColumn}>
           {selectedSkill ? (
             <div className={styles.skillCard}>
               <h3>{selectedSkill.title}</h3>
-              <p><strong>Experience:</strong></p>
+              <p>
+                <strong>Experience:</strong>
+              </p>
               <p>{selectedSkill.experience}</p>
-              <p><strong>Projects:</strong></p>
+              <p>
+                <strong>Projects:</strong>
+              </p>
               <p>{selectedSkill.projects}</p>
             </div>
           ) : (
